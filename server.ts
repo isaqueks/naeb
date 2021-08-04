5b79b12b234d535043bb8759d3cc6c10bfe78a40
Isaque <iskschluter@gmail.com>
2021-08-04 18:32:37 -0300

    add body-parser, fileUpload and cors

--- server.ts
+++ server.ts
@@ -1,8 +1,17 @@
 import express = require('express');
+import fileupload = require('express-fileupload');
+import bodyParser = require('body-parser');
+import cors = require('cors');
 import path = require('path');
+
 import { manageRoutes } from './ultraXSrc/api/routesManager';
+
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
+app.use(fileupload());
+app.use(bodyParser.json());
 
 manageRoutes(app, path.join(__dirname, './routes'));
 
