d17a9d68529cfdbe3083040f5b85556fe70a2dcd
Isaque <iskschluter@gmail.com>
2021-08-24 13:10:40 -0300

    add exports to index file

--- README.md
+++ README.md
@@ -48,9 +48,9 @@ const PORT = 3000;
 
 const server = new UltraX(PORT, path.join(__dirname, './routes'));
 server
-    .listen(() => console.log(`Server listening at http://localhost:${PORT}`))
     .useBodyParser()
     .useCors()
     .useFileUpload()
+    .listen(() => console.log(`Server listening at http://localhost:${PORT}`))
 
 ```
--- index.ts
+++ index.ts
@@ -1,9 +1,17 @@
+import CrudModel from "./build/ultraXSrc/genericCrud/src/CrudModel";
 import { ApiFunctionHandler } from "./ultraXSrc/api/apiFunctionHandler";
 import ApiRoute from "./ultraXSrc/api/apiRoute";
 import ApiRouteExecutor from "./ultraXSrc/api/apiRouteExecutor";
 import ApiTemplate from "./ultraXSrc/api/apiTemplate";
 import respondJSON from "./ultraXSrc/api/templates/respondJSON";
 import respondPlain from "./ultraXSrc/api/templates/respondPlain";
+import Field from "./ultraXSrc/genericCrud/src/Field";
+import ObjectData from "./ultraXSrc/genericCrud/src/ObjectData";
+import ObjectModel from "./ultraXSrc/genericCrud/src/ObjectModel";
+import ValuableField from "./ultraXSrc/genericCrud/src/ValuableField";
+import PowerSQL from "./ultraXSrc/powersql/src/powerSql";
+import PowerSQLDefaults from "./ultraXSrc/powersql/src/powerSqlDefaults";
+import { PowerSQLTable, PowerSQLTableColumn } from "./ultraXSrc/powersql/src/table";
 import RouteManager from "./ultraXSrc/routeManager";
 import UltraX from "./ultraXSrc/ultrax";
 
@@ -21,5 +29,16 @@ export {
     ApiRouteExecutor,
 
     RouteManager,
+
+    CrudModel,
+    ObjectModel,
+    ObjectData,
+    Field,
+    ValuableField,
+
+    PowerSQL,
+    PowerSQLDefaults,
+    PowerSQLTable,
+    PowerSQLTableColumn
     
 }
