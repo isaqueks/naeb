bc1c6baae508ff35da5ce97548977b010ff1b66a
Isaque <iskschluter@gmail.com>
2021-08-24 13:13:01 -0300

    Add exports to index file

--- ultraXSrc/db/databaseManager.d.ts
+++
@@ -1,8 +0,0 @@
-export default function Database(dbFile: any): {
-    db: any;
-    runSql: (query: string, params?: Array<any>, callback?: Function) => void;
-    getSql: (query: string, params?: Array<any>, callback?: Function) => void;
-    allSql: (query: string, params?: Array<any>, callback?: Function) => void;
-    Select: (query: string, params?: Array<any>, callback?: Function) => void;
-    promise: (query: string, params?: Array<any> | any) => Promise<any[]>;
-};
