43b1877b86b1e308595967180865407178b51f53
Isaque <iskschluter@gmail.com>
2021-08-25 18:47:57 -0300

    Add TSDoc

--- ultraXSrc/api/apiRoute.ts
+++ ultraXSrc/api/apiRoute.ts
@@ -6,8 +6,29 @@ import ApiTemplate from './apiTemplate';
  * should have
  */
 export default interface ApiRoute {
+
+    /**
+     * The route handler function
+     */
     apiFn: ApiFunctionHandler;
+
+    /**
+     * The route URL (Relative path will be used if not specified)
+     */
     route?: string;
-    method?: string;
+    /**
+     * The HTTP method
+     */
+    method?: 'get' | 'post' | 'all';
+
+    /**
+     * Optional - The template function.
+     * A template function may add extra stuff
+     * to the returned value, and may also
+     * respond the request with the thrown error
+     * if any.
+     * respondJSON and respondPlain are templates.
+     * If not specified, respondJSON would be used.
+     */
     template?: ApiTemplate;
 }
