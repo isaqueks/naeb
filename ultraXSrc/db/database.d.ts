184430fa48164aad166646cad9a8ca1b42f1b66e
Isaque <iskschluter@gmail.com>
2021-10-20 17:18:26 -0300

    Remove sqlite3

--- ultraXSrc/db/database.d.ts
+++
@@ -1,6 +0,0 @@
-import DatabaseManager from './databaseManager';
-declare const SQLiteDatabase: {
-    db: any;
-    promise: (query: string, params?: any) => Promise<any[]>;
-};
-export { DatabaseManager, SQLiteDatabase };
--- ultraXSrc/db/databaseManager.d.ts
+++
@@ -1,4 +0,0 @@
-export default function Database(dbFile: any): {
-    db: any;
-    promise: (query: string, params?: Array<any> | any) => Promise<any[]>;
-};
